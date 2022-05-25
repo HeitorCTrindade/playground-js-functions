7115e3af5dec912d4437016686c2e0ab631a7ae3
Heitor C.T <heitor-trindade@hotmail.com>
2022-05-24 23:11:10 -0300

    Exercicios Bonus Finalizados | Espero que alguem veja esse commit =)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,61 @@
+function mostFrequencyNumber(numbers) {
+  let contMFrequency = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    let contTemp = 0;
+    for (let j = 0; j < numbers.length; j += 1) {
+      if (numbers[i] === numbers[j]) {
+        contTemp += 1;
+      }
+    }
+    if (contTemp > contMFrequency) {
+      contMFrequency = contTemp;
+    }
+  }
+  return contMFrequency;
+}
+
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// eslint-disable-next-line complexity
+function generatePhoneNumber(telNumber) {
+  let formatNumber = "";
+
+  if (telNumber.length !== 11) {
+    return ('Array com tamanho incorreto.');
+  }
+  if (mostFrequencyNumber(telNumber) >= 3) {
+    return ('não é possível gerar um número de telefone com esses valores');
+  }
+  for (let i = 0; i < telNumber.length; i += 1) {
+    formatNumber += telNumber[i];
+    if (telNumber[i] < 0 || telNumber[i] > 9) {
+      return ('não é possível gerar um número de telefone com esses valores');
+    }
+  }
+  return (`(${formatNumber.slice(0, 2)}) ${formatNumber.slice(2, 7)}-${formatNumber.slice(7, 11)}`);
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let sumLine = lineA + lineB;
+  let subLine = Math.abs(lineA - lineB);
+  if (lineC < sumLine && lineC > subLine) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drinks) {
+  let waterDriks = 0;
+  for (let i = 0; i < drinks.length; i += 1) {
+    if (+drinks[i]) {
+      waterDriks += +drinks[i];
+    }
+  }
+  if (waterDriks > 1) {
+    return (`${waterDriks} copos de água`);
+  }
+  return (`${waterDriks} copo de água`);
 }
 
 module.exports = {
